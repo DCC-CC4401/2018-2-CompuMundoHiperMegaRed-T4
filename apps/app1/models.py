e8aa33a3ba4cc752b8ba2fe0fe58aea49689ac8e
Rodrigo <rodrigof96@gmail.com>
2018-11-24 17:47:02 -0300

    Agregado modelo Notas

--- apps/app1/models.py
+++ apps/app1/models.py
@@ -46,7 +46,17 @@ class Coevaluacion(models.Model):
     estadoChoices = (('Abierta', 'abierta'), ('Cerrada', 'cerrada'), ('Publicada', 'publicada'),)
     estado = models.CharField(max_length=9, choices=estadoChoices, default="cerrada")
 
+    class Meta:
+        unique_together = ["curso", "nombre"]
+
 
 class Pregunta(models.Model):
     contenido = models.TextField(max_length=300)
     ponderacion = models.FloatField()
+
+
+class Notas(models.Model):
+    coevaluacion = models.ForeignKey(Coevaluacion, on_delete=models.CASCADE)
+    nota = models.FloatField()
+    alumno = models.ForeignKey(User, on_delete=models.CASCADE)
+
